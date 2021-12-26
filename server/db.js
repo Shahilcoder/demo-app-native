ca8a2880b0d6b6fe2cbd8799bc62ed2c27e4f223
Shahilcoder <shahilcoder@gmail.com>
2021-12-26 17:20:56 +0530

    fix mysql connection

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-// const path = require('path');
+// require("dotenv").config();
+const path = require('path');
 const mysql = require("mysql");
 
 const con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: 3306
 });
--- server/server.js
+++ server/server.js
@@ -25,37 +25,42 @@ app.get('/', (req, res) => {
 
 app.post('/signup', (req, res) => {
 	let user = req.body;
-	con.connect(err => {
-		if (err) throw err;
-		console.log("connected");
-		con.query("USE sql6461103", (err, result) => {
-			if (err) console.log(err);
-		});
-		con.query("INSERT INTO user (email, password) VALUE " + "('" + user.email + "', '" + user.password + "')", (err, result) => {
-			if (err) console.log(err);
-			console.log('User created!');
-			res.send("User created");
-		});
-	})
+	
+	if (con.state === "disconnected") {
+	    con.connect(err => {
+		    if (err) throw err;
+		    console.log("connected");
+	    });
+	}
+
+	con.query("INSERT INTO user (email, password) VALUE " + "('" + user.email + "', '" + user.password + "')", (err, result) => {
+		if (err) console.log(err);
+		console.log('User created!');
+		res.send("User created");
+	});
 });
 
 app.post('/login', (req, res) => {
 	let user = req.body;
-	con.connect(err => {
-		if (err) throw err;
-		console.log("connected");
-		con.query("USE sql6461103", (err, result) => {
-			if (err) console.log(err);
-		});
-		con.query("SELECT email, password FROM user WHERE email = '" + user.email + "'", (err, result) => {
-			if (err) console.log(err);
+
+	if (con.state === "disconnected") {
+	    con.connect(err => {
+		    if (err) throw err;
+		    console.log("connected");
+	    });
+	}
+		
+	con.query("SELECT email, password FROM user WHERE email = '" + user.email + "'", (err, result) => {
+		if (err) console.log(err);
 			
-			if (result[0].password === user.password) {
-				console.log("Successfully Logged in!");
-				res.send("Successfully logged in!");
-			}
-		})
-	})
+		if (result[0].password === user.password) {
+			console.log("Successfully Logged in!");
+			res.status(200).send("Successfully logged in!");
+		} else {
+			console.log("Error: Password incorrect");
+			res.status(404).send("Error: Password incorrect");
+		}
+	});
 });
 
 app.listen(process.env.PORT, () => {
